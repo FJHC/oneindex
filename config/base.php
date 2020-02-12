c5b6eda90c4ed3aa14aba61d2f4493c0d605cb1c
河川 <30566834+hechuan4@users.noreply.github.com>
2020-02-12 13:44:04 +0800

    heroku
    heroku-包含个人配置信息的

---
+++ config/base.php
@@ -0,0 +1,106 @@
+<?php return array (
+  'site_name' => '里番',
+  'password' => '1479696753+o',
+  'style' => 'material',
+  'onedrive_root' => '/',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 600,
+  'cache_refresh_time' => 600,
+  'root_path' => '',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'mrjRVL67-!lftrKKPX631:;',
+  'client_id' => 'f18e2a54-6d4a-4d61-8ab3-fbd97157aafe',
+  'redirect_uri' => 'https://oneindex.github.io/',
+  'refresh_token' => 'OAQABAAAAAABeAFzDwllzTYGDLh_qYbH8jWFYSov8u4pkDbwehaKaoCbuL14daPrkdsApgkLEbIrxSCu4kD3Ec-qMNSDO89xR7hSG9fJiR-zAGPhUnhoJ1H_e9sSscgkGGx3aJ2XNlRuAkQDdavUXQEkahDwAEcT-DZCtn-bnzAfOolrcr1HvgNSUuf7T5IWQm0Ae_giAu-nkrUS2Oa57SeC2XLeSmz92U_xI1b1kTkjtcxMMW1IGOo-sXUz5O0qtuR7hpcWFKZZcXIkQTSyaZ8TwQm1dap0OHfl0sLqKDdSNDpinAjfbz9Kgjc0M11PjLwZoHE8pTX6UHR0FGol8WC8Nbn12lAuqoJhzfA2pUF_zyeU8gqSD0KpaV5V_GIb9tcHU-588nu2no73H834RdB7gQ_1aFIxLc5ilYAHg1ip-oM5TV_w_9iS1hEIpVyDlfObfQ7z5KdrLTCwViIeLMwuT6Cc2P2kwsRhaoon2N76csDkNgKNL2e3nnhphk8C2lVXObRYWY_IyuJHVcUWh-riZPzhAaYXe8KUqGGAaEpOYQAi7t-EALEU4zJmbQPtGZxTlRa6IYsRt3uXCu-nrV4omKS_YqRVN8QzQOL6Q_QzdEX6Ubc2l_zK9RID7eC9-dO32thJTdix3O5HVJQu6ez9_8hTYkqAWCU6lMakl219IPSAl9IPHW_y_-s-zlldu0xynEj3xSQYnLXxS04XsROgbqye-H8YKRCYQg6kMRbtQI4s-DdNToBFV6CWcHGBVOfYAO97e5i93UWr9K6-mY6t_2Ui6ExEc9DQtrTUKa3ptkkHBVnt5enPlbcHNVWrux9yX5deyVLogAA',
+  'onedrive_hide' => '',
+  'onedrive_hotlink' => '',
+);
